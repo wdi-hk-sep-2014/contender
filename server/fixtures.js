6d407a03500ee8a7a28d39b782e2097a6277115a
songyeep <songyeep@gmail.com>
2014-12-05 19:15:28 +0800

    added comments collection and pub sub

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,15 +1,55 @@
 if (Posts.find().count() === 0) {
+  var now = new Date().getTime();
+
+
+  // dummy users
+  var songId = Meteor.users.insert({
+    profile: {name: "Songyee Park"}
+
+  });
+  var song = Meteor.users.findOne(songId);
+
+  var harryId = Meteor.users.insert({
+    profile: {name: "Harry Potter"}
+  });
+  var harry = Meteor.users.findOne(harryId);
+
+
+  var chessId = Posts.insert({
+    title: "I'll beat you so hard",
+    activity: "Chess",
+    description: "Lan Kwai Fong, this weekend, around 7pm. You up for this?",
+    new Date(now - 7 * 3600 * 1000)
+  });
+
+  Comments.insert({
+    postId: chessId,
+    userId: harry._id,
+    author: harry.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Bring it onnnnnn.'
+  });
+
+  Comments.insert({
+    postId: chessId,
+    userId: song._id,
+    author: song.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: "No using magic! Fair play!"
+  })
 
   Posts.insert({
-    title: "I'll beat you so hard"
+    title: "Currently undefeated in the game of Tic-Tac-Toe",
+    activity: "Tic-Tac-Toe",
+    description: "Cyberport, 3F, tomorrow. Bring pen and paper, sucka!",
+    submitted: new Date(now - 10 * 3600 * 1000)
   });
 
   Posts.insert({
-    title: "Currently undefeated in the game of Tic-Tac-Toe"
-  });
-
-  Posts.insert({
-    title: "Seeking a Tennis rival"
+    title: "Seeking a Tennis rival",
+    activity: "Tennis",
+    description: "There's a tennis court behind my apartment at Central. 7pm, loser buys dinner!",
+    submitted: new Date(now - 12 * 3600 * 1000)
   });
 
 }
