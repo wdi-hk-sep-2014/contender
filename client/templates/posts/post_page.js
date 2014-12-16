548c3d2347bbd406f18d5b554b54dbeb6580485b
songyeep <songyeep@gmail.com>
2014-12-16 18:17:28 +0800

    added UI tweaks to joining events

--- client/templates/posts/post_page.js
+++ client/templates/posts/post_page.js
@@ -1,9 +1,43 @@
 Template.postPage.helpers({
   comments: function() {
     return Comments.find({postId: this._id});
+  },
+
+  joinedSpec: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.spectators, userId)) {
+      return 'Join as Spectator';
+    } else {
+      return 'You are a Spectator';
+    }
+  },
+
+  joinedSpec2: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.spectators, userId)) {
+      return 'upSpec btn btn-primary';
+    } else {
+      return 'btn btn-success';
+    }
+  },
+
+  joinedChall: function(){
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.challengers, userId)) {
+      return 'Join as Challenger';
+    } else {
+      return 'You are a Challenger';
+    }
+  },
+
+  joinedChall2: function(){
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.challengers, userId)) {
+      return 'upChall btn btn-danger';
+    } else {
+      return 'btn btn-success';
+    }
   }
-
-
 });
 
 Template.postPage.events({
