ed6be892cc6fb113f3d2c37a389a21d4fcb1dd63
songyeep <songyeep@gmail.com>
2014-12-17 20:59:52 +0800

    USBAT select challenger from list

--- client/templates/posts/post_challengers.js
+++ client/templates/posts/post_challengers.js
@@ -1,13 +1,14 @@
 Template.postChallengers.helpers({
   challengers: function() {
-    console.log(this);
     return this.challengers;
   }
 });
 
+
+
+
 Template.challenger.helpers({
   userName: function() {
-    console.log("userId: " + this);
     userId = this.toString();
 
     user = Meteor.users.findOne({_id: userId});
@@ -16,13 +17,5 @@ Template.challenger.helpers({
 });
 
 Template.challenger.events({
-  'click .selectChallenger': function(e) {
-    e.preventDefault();
-
-    if (confirm("Select this Challenger?")) {
-
-      Router.go('postPage');
-    }
-  }
 
 });
